623fc5d382e3e3a3bedd5e446e7ffa763e64de0a
KamleshTechuz <kamlesh@techuz.com>
2024-11-29 14:56:52 +0530

    2nd image removed

--- src/components/Proj.tsx
+++ src/components/Proj.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 const Card = ({ project }: any) => {
   return (
     <div className="flex-shrink-0 w-full">
-      <div className=" p-6 space-y-4 flex flex-col justify-center relative rounded-lg ">
-        <div className="flex gap-12 w-full h-full">
+      <div className="p-6 space-y-4 flex flex-col justify-center relative rounded-lg">
+        <div className="flex flex-col lg:flex-row gap-12 w-full h-full">
           {/* Adjust height as needed */}
-          <div className="flex flex-col gap-10 w-1/2">
-            <div className=" flex items-center justify-between">
+          <div className="flex flex-col gap-10 w-full lg:w-1/2">
+            <div className="flex items-center justify-between">
               <h3 className="text-3xl font-semibold text-green-400">
                 <code>{project?.title}</code>
               </h3>
@@ -23,39 +23,27 @@ const Card = ({ project }: any) => {
             <div>
               <code>{project?.description}</code>
             </div>
+            <div className="flex flex-col justify-center gap-6 w-full lg:w-1/2">
+            <code className="text-2xl">Technologies</code>
+            <div className="flex flex-wrap gap-4">
+              {project?.technologies.map((tch: string, i: number) => (
+                <code
+                  className="rounded-full py-2 px-4 bg-gradient-to-r from-green-500 to-green-700"
+                  key={i}
+                >
+                  {tch}
+                </code>
+              ))}
+            </div>
           </div>
-          <div className="w-1/2">
+          </div>
+          <div className="w-full lg:w-1/2">
             <Image
               src={project.images[0]}
               alt={project?.title}
-              className="inset-0 w-full   rounded-lg shadow-lg"
+              className="inset-0 w-full rounded-lg shadow-lg"
               style={{ zIndex: 3, transform: "translateY(0px)" }}
             />
-          </div>
-        </div>
-      </div>
-      <div className=" p-6 space-y-4 flex flex-col justify-center relative rounded-lg ">
-        <div className="flex gap-12 w-full h-full">
-          {/* Adjust height as needed */}
-          <div className="w-1/2">
-            <Image
-              src={project.images[1]}
-              alt={project?.title}
-              className="inset-0 w-full   rounded-lg shadow-lg"
-              style={{ zIndex: 3, transform: "translateY(0px)" }}
-            />
-          </div>
-          <div className="flex flex-col justify-center gap-6 w-1/2">
-          <code className="text-2xl">
-          Technologies
-          </code>
-            <div className="flex gap-4">
-              {
-                project?.technologies.map((tch: string, i: number) => (
-                  <code className="rounded-full py-2 px-4 bg-gradient-to-r from-green-500 to-green-700" key={i}>{tch}</code>
-                ))
-              }
-            </div>
           </div>
         </div>
       </div>
